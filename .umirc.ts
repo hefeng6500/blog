1a493917047051a2496fd306b965de9cfbb8fb3c
hefeng6500 <2443992009@qq.com>
2020-11-25 20:17:56 +0800

    fix

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
+  base: '/blog/',
+  description: "hefeng6500的博客"
   // more config: https://d.umijs.org/config
 });
